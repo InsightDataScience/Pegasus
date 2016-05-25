b61c3dd726d085ad1d6c0f2a7b5c5cbb92216bb6
Aaron Binns <abinns@taskrabbit.com>
2016-05-25 14:24:12 -0700

    Toggle association of EIPs based on AWS_ELASTIC_IPS environment variable.

--- util.sh
+++ util.sh
@@ -377,14 +377,16 @@ function run_instances {
     tag_resources Role ${role} ${INSTANCE_IDS}
   fi
 
-  for instance_id in ${INSTANCE_IDS}; do
-    association_status=$(allocate_and_associate_eip ${instance_id})
-    if [[ ${association_status} != eipassoc-* ]]; then
-      echo -e "${color_red}Elastic IP not associated with the instance ${instance_id}${color_norm}"
-    else
-      echo -e "${color_green}Elastic IP associated with the instance ${instance_id}${color_norm}"
-    fi
-  done
+  if [ ! -z ${AWS_ELASTIC_IPS} ]; then
+    for instance_id in ${INSTANCE_IDS}; do
+      association_status=$(allocate_and_associate_eip ${instance_id})
+      if [[ ${association_status} != eipassoc-* ]]; then
+        echo -e "${color_red}Elastic IP not associated with the instance ${instance_id}${color_norm}"
+      else
+        echo -e "${color_green}Elastic IP associated with the instance ${instance_id}${color_norm}"
+      fi
+    done
+  fi
 }
 
 function check_remote_folder {
